3d6efe6b401e750d1cc6b6bc3d6996160457ec8b
Greg Pintilie <greg@5K.local>
2017-03-07 11:38:58 -0500

    change folder structure

--- __init__.py
+++
@@ -1,5 +0,0 @@
-dev_menus = False       # Include under-development menus.
-timing = False          # Report execution times for optimizing code.
-seggerVersion = '1.9.5'
-
-from regions import Segmentation, Region, SelectedRegions
