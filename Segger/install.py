bc85341931e488121964ede49b8b9e3ca0797fc5
Greg Pi <gregdp@gmail.com>
2019-05-24 09:43:19 -0700

    updated install script to work with python3 and Windows installs

--- Segger/install.py
+++ Segger/install.py
@@ -1,68 +1,130 @@
 import sys, os, shutil
 
-#li = sys.argv.index ( "install.py" )
-#print li
-
-
 if len(sys.argv) != 2 :
-    print ""
-    print "Please add the path where Chimera is installed, e.g.:"
-    print "   python install.py /home/greg/applications/Chimera"
-    print ""
+    print ("")
+    print ("Please add the path where Chimera is installed, e.g.:")
+    print ("   python install.py /home/greg/applications/Chimera")
+    print ("")
 
-    exit()
+    sys.exit(0)
     
 
-print ""
+print ("")
 
+
+
+sharePaths = []
+def FindShare (path, lev) :
+
+    try :
+        fs = os.listdir (path)
+    except :
+        fs = []
+
+    for f in fs :
+        atPath = os.path.join ( path, f )
+        if os.path.isdir ( atPath ) :
+            #for i in range(lev+1) :
+            #    print "  ",
+            #print f
+            if f == "share" and "chimera" in atPath.lower() :
+                #print " - found: ", atPath
+                sharePaths.append ( atPath )
+                return
+            else :
+                FindShare ( atPath, lev+1 )
+
+
+
+
+
+
+# Mac...
 opath1 = os.path.join ( sys.argv[1], "Contents" )
 opath1 = os.path.join ( opath1, "Resources" )
 opath1 = os.path.join ( opath1, "share" )
 
+# Unix...
 opath2 = os.path.join ( sys.argv[1], "share" )
 
 didInstall = False
 
-for opath in [opath1, opath2] :
+chimeraPath = sys.argv[1]
+if not os.path.isdir ( chimeraPath ) :
+    print ("")
+    print ("The specified Chimera path '%s' doesn't seem to exist" % chimeraPath )
+    print (" - please check and try again")
+    print ("")
+    sys.exit(0)
+
+if not "chimera" in chimeraPath.lower() :
+    print ("")
+    print ("The specified path '%s' doesn't seem to be for Chimera" % chimeraPath )
+    print (" - please check and try again")
+    print ("")
+    sys.exit(0)
+
+
+#print ("finding...")
+FindShare (sys.argv[1], 0)
+sharePath = None
+for p in sharePaths :
+    if sharePath == None or len(p) < len(sharePath) :
+        sharePath = p
+#print (sharePath)
+
+#exit()
+
+if sharePath == None :
+    print ("")
+    print ("Could not find the 'share' folder in the specified path" )
+    print (" - please check that the path points to Chimera and try again")
+    print ("")
+    sys.exit(0)
+
+
+
+for opath in [sharePath] :
 
     if os.path.isdir( opath ) :
         opath = os.path.join ( opath, "Segger" )
-    
+
         if os.path.isdir( opath ) :
-            print " - removing previous Segger:", opath
+            print (" - removing previous Segger:" + opath)
             try :
                 shutil.rmtree(opath)
             except :
                 pass
-        
+
         #print " - copying from:", os.getcwd()
-        print " - copying . ->", opath
+        print (" - copying . ->" + opath )
     
         try :
-            shutil.copytree ( os.getcwd(), opath )  
+            shutil.copytree ( os.getcwd(), opath )
             didInstall = True
         except :
-            print "Could not copy to ", opath
-            print " 1. please check if you have write access"
-            print " 2. try with sudo python install.py <path to Chimera>"
-            print ""
+            print ("")
+            print ("-----------------------------------------------------")
+            print ("Problem: Could not copy to:", opath)
+            print (" 1. please check if you have write access")
+            print (" 2. try with sudo python install.py <path to Chimera>")
+            print ("-----------------------------------------------------")
+            print ("")
             break
-
+            
         didInstall = True
 
-if didInstall :
+if didInstall :            
 
-    print ""
-    print "Installation complete."
-    print ""
-    print "To use:"
-    print " 1. Please restart Chimera."
-    print " 2. Select Tools -> Volume Data -> Segment Map"
-    print ' 3. Please note that on Mac OS, you may see the message "Chimera is damaged and cannot be opened." Please see the following link for the solution: https://www.santoshsrinivas.com/disable-gatekeeper-in-macos-sierra/'
-    print ' 4. More info: https://cryoem.slac.stanford.edu/ncmi/resources/software/Segger'
-    print ' 5. Tutorials: https://github.com/gregdp/segger/tree/master/tutorials'
-    print ' 6. For other questions/comments/suggestions, please contact gregp@slac.stanford.edu'
-    print ""
+    print ("")
+    print ("------------------------")
+    print ("Installation successful.")
+    print ("------------------------")
+    print ("")
+    print ("To use:")
+    print (" 1. Restart Chimera.")
+    print (" 2. Select Tools -> Volume Data -> Segment Map")
+    print ("")
     
     #wh = os.path.join ( os.getcwd(), "install.html" )
     #import webbrowser
@@ -70,11 +132,13 @@ if didInstall :
 
 
 else :
-    print ""
-    print 'Chimera not found in "' + sys.argv[1] + '"'
-    print " 1. please check the path"
-    print " 2. remember you can auto-complete while typing the path with the tab key"
-    print " 3. if issue persists, please report to gregp@slac.stanford.edu"
-    print ""
+    print ("")
+    print ("-----------------------------------------------------------------------")
+    print ("Problem: Could not find 'share' folder in '" + sys.argv[1] + "'")
+    print (" 1. please check the path")
+    print (" 2. remember you can auto-complete while typing the path with <tab>")
+    print ("-----------------------------------------------------------------------")
+    print ("")
 
+        
 
