2daad2b03f4a5dd3edb4b1626cf8df4c5c24b9ef
Greg Pi <gregdp@gmail.com>
2021-01-30 17:30:02 -0500

    change to download zip file format

--- segger_plugin/Segger/__init__.py
+++
@@ -1,6 +0,0 @@
-dev_menus = False       # Include under-development menus.
-timing = False          # Report execution times for optimizing code.
-showDevTools = False
-seggerVersion = '2.5.4'
-
-from regions import Segmentation, Region, SelectedRegions
