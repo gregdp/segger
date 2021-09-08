d4c112fd43f0b088145e225f976335800874ebe5
Greg Pintilie <gregdp@gmail.com>
2021-09-08 19:49:01 -0400

    fix in mapq calculating Q-scores for selected atoms

--- Segger/__init__.py
+++ Segger/__init__.py
@@ -1,7 +1,7 @@
 dev_menus = False       # Include under-development menus.
 showDevTools = False    # some new also under-development tools
 timing = False          # Report execution times for optimizing code.
-seggerVersion = '2.8.1'
-mapqVersion = '1.8.1'
+seggerVersion = '2.8.2'
+mapqVersion = '1.8.2'
 
 from regions import Segmentation, Region, SelectedRegions
